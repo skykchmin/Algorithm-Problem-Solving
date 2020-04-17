0fd5b40091c18fee696faaf36049d9613d092268
skykchmin <skykchmin@naver.com>
2020-04-17 12:41:37 +0900

    checkio_Say Hi.py
    checkio_Say Hi.py
    
    https://py.checkio.org/en/mission/say-history/
    
    [200417]

---
+++ checkio_say_hi.py
@@ -0,0 +1,12 @@
+# 1. on CheckiO your solution should be a function
+# 2. the function should return the right answer, not print it.
+
+def say_hi(name: str, age: int) -> str:
+    
+    return "Hi. My name is {0} and I'm {1} years old".format(name, age)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", "First"
+    assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", "Second"
+    print('Done. Time to Check.')
