928d1b329cf5adf46a1e37e91d5554ea909c3b75
skykchmin <skykchmin@naver.com>
2020-04-29 23:56:46 +0900

    checkio_split_pairs.py
    checkio_split_pairs.py
    
    [200429]

---
+++ checkio_split_pairs.py
@@ -0,0 +1,18 @@
+import itertools
+import operator
+
+def split_pairs(a):
+    connection_character = itertools.chain(a, '_')
+    return map(operator.add, connection_character, connection_character)
+
+def split_pairs2(a):
+    len_split_pairs = len(a)
+
+    if len_split_pairs == 0:
+        return []
+    
+    if len_split_pairs == 1:
+        return [a + '_']
+
+    else:
+        return [a[:2]] + split_pairs2(a[2:])
