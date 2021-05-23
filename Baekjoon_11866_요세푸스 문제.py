114f0ef96be5e0e041fd09573d5bac693d6f5587
skykchmin <skykchmin@naver.com>
2021-05-23 14:15:20 +0900

    [210523]Baekjoon_11866_요세푸스 문제.py
    [210523]Baekjoon_11866_요세푸스 문제.py

--- Baekjoon_11866_요세푸스 문제.py
+++ Baekjoon_11866_요세푸스 문제.py
@@ -15,4 +15,17 @@ print("<%s>" %(", ".join(result)))
 
 
 
-   
+# 다른 사람 풀이
+N, K = map(int, input().split()) 
+circular_list = [] 
+answer = []
+for i in range(N): circular_list.append(i+1) 
+popNum = 0 
+
+while len(circular_list) > 0:
+    popNum = (popNum + (K-1)) % len(circular_list) 
+    popElemnet = circular_list.pop(popNum) 
+    answer.append(str(popElemnet)) 
+    
+print("<%s>" %(", ".join(answer)))
+
