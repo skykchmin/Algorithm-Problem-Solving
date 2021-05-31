fb75ad1a6b60008fec9de4adcb082f745e7a2681
skykchmin <skykchmin@naver.com>
2021-05-31 23:42:32 +0900

    [210531]이코테_BFS.py
    [210531]이코테_BFS.py

--- programmers_정수 제곱근 판별.py
+++ programmers_정수 제곱근 판별.py
@@ -1,8 +1,12 @@
 n = int(input())
 
 def solution(n):
-    print(n**2)
-    return
-
+    result = 0
+    temp = n**(1/2)
+    if temp**2 == n:
+        result = int((temp + 1) ** 2)
+        return result
+    else:
+        return -1
 
 solution(n)
