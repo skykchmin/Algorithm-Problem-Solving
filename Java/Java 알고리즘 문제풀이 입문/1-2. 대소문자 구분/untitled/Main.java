49ead8588a7778b215affc0865290a56f1b3785b
skykchmin <skykchmin@naver.com>
2024-03-31 23:35:30 +0900

    [240331] add: 1-2. 대소문자 변환

---
+++ Java/Java 알고리즘 문제풀이 입문/1-2. 대소문자 구분/untitled/Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution2(String inputStr){
+        char[] chars = new char[inputStr.length()]; // 크기 조정
+
+        for(int i = 0; i < inputStr.length(); i++){
+            char s = inputStr.charAt(i); // 각 문자열의 문자를 가져옴
+
+            if(Character.isUpperCase(s)){
+                chars[i] = Character.toLowerCase(s); // 대문자이면 소문자로 변환하여 배열에 저장
+            } else {
+                chars[i] = Character.toUpperCase(s); // 소문자이면 대문자로 변환하여 배열에 저장
+            }
+        }
+
+        String result = String.valueOf(chars);
+
+        return result;
+    }
+
+    public String solution(String inputStr){
+        StringBuilder resultBuilder = new StringBuilder();
+
+        for(char s : inputStr.toCharArray()){
+            if(Character.isUpperCase(s)){
+                resultBuilder.append(Character.toLowerCase(s));
+            } else {
+                resultBuilder.append(Character.toUpperCase(s));
+            }
+        }
+
+        return resultBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.next();
+        sc.close();
+
+        String solution = T.solution(inputStr);
+        System.out.println(solution);
+    }
+}
