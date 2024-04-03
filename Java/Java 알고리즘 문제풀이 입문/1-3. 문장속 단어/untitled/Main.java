e6f2af44c269a874dd45f7c8e3e0b663fcb3a016
skykchmin <skykchmin@naver.com>
2024-04-03 23:46:39 +0900

    [240404] add: 1-3. 문장속 단어(추가)

--- Java/Java 알고리즘 문제풀이 입문/1-3. 문장속 단어/untitled/Main.java
+++ Java/Java 알고리즘 문제풀이 입문/1-3. 문장속 단어/untitled/Main.java
@@ -17,6 +17,38 @@ public class Main {
         return answer;
     }
 
+    public String solutionLecture(String inputStr){
+        String answer = "";
+        int max = Integer.MIN_VALUE;
+        String[] s = inputStr.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if(len > max){
+                max = len;
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+
+    public String solutionLecture2(String inputStr){
+        String answer = "";
+        int m = Integer.MIN_VALUE, pos;
+        while ((pos = inputStr.indexOf(" ")) != -1){
+            String tmp = inputStr.substring(0, pos);
+            int len = tmp.length();
+            if(len > m){
+                m = len;
+                answer = tmp;
+            }
+            inputStr = inputStr.substring(pos + 1);
+        }
+        if(inputStr.length() > m) answer = inputStr;
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
