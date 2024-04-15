bf656a45852f36bf0cb8fb90a2b8fd79ccc1075d
skykchmin <skykchmin@naver.com>
2024-04-15 22:51:07 +0900

    [240415] 2-8. 등수 구하기

---
+++ Java/Java 알고리즘 문제풀이 입문/2-8. 등수 구하기/Main/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public int[] solution(int n, int[] inputNumbers) {
+
+        Integer[] arr = Arrays.stream(inputNumbers.clone()).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, Collections.reverseOrder());
+        List<Integer> rankList = Arrays.stream(arr)
+                .mapToInt(Integer::intValue)
+                .boxed()
+                .collect(Collectors.toList());
+
+        int[] answers = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int index = rankList.indexOf(inputNumbers[i]);
+            answers[i] = index + 1;
+        }
+
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] inputNumbers = new int[n];
+        for(int i = 0; i < n; i++){
+            inputNumbers[i] = sc.nextInt();
+        }
+
+        sc.close();
+        int[] answer = T.solution(n, inputNumbers);
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
