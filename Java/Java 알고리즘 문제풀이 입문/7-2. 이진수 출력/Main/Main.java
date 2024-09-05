e72064b2663f11e281df83795da0cdf570a935b7
skykchmin <skykchmin@naver.com>
2024-09-05 21:59:25 +0900

    [8-2] 이진수 출력(재귀)

---
+++ Java/Java 알고리즘 문제풀이 입문/7-2. 이진수 출력/Main/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        T.DFS(N);
+    }
+
+    private void DFS(int n) {
+        if(n == 1){
+            System.out.print(1);
+            return;
+        }
+
+        DFS(n / 2);
+        System.out.print(n % 2);
+    }
+}
+
